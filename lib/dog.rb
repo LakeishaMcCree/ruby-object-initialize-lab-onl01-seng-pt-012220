e720f77c3d6edf437face69cb22ef48e70e859ba
Lakeisha McCree <lmccree215@icloud.com>
2020-03-17 04:22:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = "Fido"
+    fido = Dog.new("Fido", "Pug")
+  end
+  
+  def breed
+    @breed = "Pug"
+    fido = Dog.new("Fido", "Pug")
+  end
+  
+  
+end
+
+    
